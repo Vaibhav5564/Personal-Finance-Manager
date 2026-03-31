9e6ed3e773f2deb5a5416c5e9687ec75ebc8ddcc
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-03-31 20:18:48 +0000

    Removed unneccessery comments

--- Personal Finance Manager/app/utils.py
+++ Personal Finance Manager/app/utils.py
@@ -1,6 +1,3 @@
-# this file is for helper functions
-# anything reusable should come here
-
 
 # -----------------------------
 # CALCULATE TOTALS
@@ -14,26 +11,5 @@ def calculate_totals(transactions):
     return income, expense, balance
 
 
-# -----------------------------
-# FORMAT CURRENCY
-# -----------------------------
-
-def format_currency(amount):
-    return f"₹{amount:,.2f}"
 
 
-# -----------------------------
-# FILTER BY CATEGORY
-# -----------------------------
-# useful if later you add filters in dashboard
-def filter_by_category(transactions, category):
-    return [t for t in transactions if t.category.lower() == category.lower()]
-
-
-# -----------------------------
-# GET RECENT TRANSACTIONS
-# -----------------------------
-# return latest N transactions (default 5)
-def get_recent_transactions(transactions, limit=5):
-    sorted_transactions = sorted(transactions, key=lambda x: x.date, reverse=True)
-    return sorted_transactions[:limit]
